2fda2198ae6f13fa59a64c95b4ea514ee34920cc
hannadabrowska <45164633+hannadabrowska@users.noreply.github.com>
2018-12-02 11:05:03 +0100

    zadanie7.ino
    arduino.ino

---
+++ zadanie7.ino
@@ -0,0 +1,32 @@
+int dioda = 13;
+int czas = 500;
+int czas2 = 1200;
+
+void setup(){
+    pinMode(dioda, OUTPUT);
+}
+
+void loop(){
+    for (int i=0; i < 3; i=i+1){
+        digitalWrite(dioda, HIGH);
+        delay(czas);
+        digitalWrite(dioda, LOW);
+        delay(czas);
+   }
+
+    for (int i=0; i < 3; i=i+1){
+           digitalWrite(dioda, HIGH);
+           delay(czas2);
+           digitalWrite(dioda, LOW);
+           delay(czas);
+    }
+
+    for (int i=0; i < 3; i=i+1){
+            digitalWrite(dioda, HIGH);
+            delay(czas);
+            digitalWrite(dioda, LOW);
+            delay(czas);
+     }
+
+     delay(5000);
+}
